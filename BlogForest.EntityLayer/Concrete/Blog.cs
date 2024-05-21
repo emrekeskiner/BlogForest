043417c04fb19884459433a123b051d004471cbd
Emre Keskiner <emrekeskiner@gmail.com>
2024-05-21 20:29:41 +0300

    Add project files.

---
+++ BlogForest.EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogForest.EntityLayer.Concrete
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string? Title { get; set; }
+        public string? ThumbnailImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }= DateTime.Now;
+        public int? ViewCount { get; set; }
+        public string? CoverImageUrl { get; set; }
+        public string? Description { get; set; }
+
+
+    }
+}
---
+++ BlogForest.EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogForest.EntityLayer.Concrete
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string? Detail { get; set; }
+        public DateTime CommentDate { get; set; }= DateTime.Now;
+        public bool Status { get; set; }=false;
+    }
+}
